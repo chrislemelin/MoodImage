08b9a40f9a5356b3e91d22799248100108d9d706
Christopher Lemelin <clemelin2@gmail.com>
2017-02-25 12:14:44 -0500

    moved more things around

--- MoodImage/Resources.cs
+++ MoodImage/Resources.cs
@@ -5,10 +5,7 @@ namespace MoodImage
 	{
 		public static readonly string APIKEY = "89b424d4445548eead8652bcc0f15ef2";
 		public static readonly string URL = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize";
-		public static readonly 
+	
 
-		public Resources()
-		{
-		}
 	}
 }
